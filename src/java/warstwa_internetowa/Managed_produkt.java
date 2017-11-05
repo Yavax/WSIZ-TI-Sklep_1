01274cf7d6c9c46ee7d455ecd92e5451bab12536
Pawel_L <yavax.dev@gmail.com>
2017-11-05 12:20:41 +0100

    Add few arguments and methods in Managed_product

--- src/java/warstwa_internetowa/Managed_produkt.java
+++ src/java/warstwa_internetowa/Managed_produkt.java
@@ -5,8 +5,10 @@
  */
 package warstwa_internetowa;
 
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import warstwa_biznesowa.Fasada_warstwy_biznesowej;
 
 /**
  *
@@ -16,10 +18,58 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class Managed_produkt {
 
-    /**
-     * Creates a new instance of Managed_produkt
-     */
-    public Managed_produkt() {
+    @EJB
+    private Fasada_warstwy_biznesowej fasada;
+    private String nazwa;
+    private String cena;
+    private String promocja;
+    private String cena_brutto;
+
+    public Managed_produkt() {   
+    }
+
+    public Fasada_warstwy_biznesowej getFasada() {
+        return fasada;
+    }
+
+    public void setFasada(Fasada_warstwy_biznesowej fasada) {
+        this.fasada = fasada;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getCena() {
+        return cena;
+    }
+
+    public void setCena(String cena) {
+        this.cena = cena;
+    }
+
+    public String getPromocja() {
+        return promocja;
+    }
+
+    public void setPromocja(String promocja) {
+        this.promocja = promocja;
+    }
+
+    public String getCena_brutto() {
+        return cena_brutto;
+    }
+
+    public void setCena_brutto(String cena_brutto) {
+        this.cena_brutto = cena_brutto;
     }
     
+    public String dodaj_produkt(){
+        String[] dane = {nazwa, cena, promocja};
+        return "rezultat1";
+    }
 }
