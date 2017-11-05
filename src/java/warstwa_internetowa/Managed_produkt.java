f66d912805fb26cc676b519d4a9e0b4425244760
Pawel_L <yavax.dev@gmail.com>
2017-11-05 11:55:58 +0100

    Create new JSF Managed Bean - Managed product

---
+++ src/java/warstwa_internetowa/Managed_produkt.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warstwa_internetowa;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author N8
+ */
+@Named(value = "managed_produkt")
+@Dependent
+public class Managed_produkt {
+
+    /**
+     * Creates a new instance of Managed_produkt
+     */
+    public Managed_produkt() {
+    }
+    
+}
